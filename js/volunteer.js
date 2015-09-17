689976b29ff5c656849b1e0380a5f681b9f4e3a1
pajowu <github@ca.pajowu.de>
2015-09-17 17:47:21 +0200

    improved Unanswered Question Fragment even more

--- js/volunteer.js
+++ js/volunteer.js
@@ -1,3 +1,5 @@
+var cats;
+var items_by_cat = {};
 function init() {
     var apisToLoad;
     var loadCallback = function() {
@@ -12,13 +14,11 @@ function init() {
     gapi.client.load('donate', 'v1', loadCallback, apiRoot);
     gapi.client.load('oauth2', 'v2', loadCallback);
 }
-
 function signin(mode, authorizeCallback) {
     gapi.auth.authorize({client_id:"760560844994-04u6qkvpf481an26cnhkaauaf2dvjfk0.apps.googleusercontent.com",
         scope: "profile", immediate: mode},
         authorizeCallback);
 }
-
 function userAuthed() {
     var request =
     gapi.client.oauth2.userinfo.get().execute(function(resp) {
@@ -27,32 +27,47 @@ function userAuthed() {
         }
     });
 }
-
 function auth() {
     signin(false, userAuthed);
 };
-
 function deauth() {
     gapi.auth.setToken(null)
     $("#signInButton").show();
     $("#signInButton").text("Sign in");
     $("#signOutButton").hide();
 };
-
 function signedIn() {
     $("#signInButton").hide();
     $("#signOutButton").show();
     jumpToPage();
 }
-
 $(document).ready(function() {
     $(window).bind( 'hashchange', function(e) {
         jumpToPage()
     });
+    $("#unanswered").on('click','#save',function(e) {
+        $("#unanswered").html("");
+        //
+        var form = $(e.target).parent();
+        NProgress.start();
+        var question = form.find("#question")[0].value;
+        var answer = form.find("#answer")[0].value;
+        var language = form.find("#language")[0].value;
+        var id = form.find("#id")[0].value;
+        var answered = form.find("#answered")[0].checked;
+        var category = form.find("#category")[0].value;
+        gapi.client.donate.faqitem.update({"id":id,"question":question, "answer":answer, "answered":answered, "language":language, "category":category}).execute(function(items) {
+            NProgress.set(0.5);
+            showUnanswered();
+        });
+    });
+    $("#unanswered").on('click','.cat',function(e) {
+        var p = $(e.target).parent().parent().parent();
+        p.find("#category").prop('value', e.target.id); 
+        p.find("#dropdownMenuTitle").text(e.target.textContent);
+    });
 });
-
 function jumpToPage() {
-
     var location = window.location.hash;
     if (location.match("^#unanswered")) {
         $('nav').removeClass('fixed');
@@ -60,8 +75,6 @@ function jumpToPage() {
         $('nav a#unanswered_link').parent().addClass('active');
         showUnanswered();
     }
-    
-
 }
 /*function loadSharing() {
     $("#home").hide();
@@ -70,42 +83,51 @@ function jumpToPage() {
     $("#map_container").hide();
 }*/
 function showUnanswered() {
+    if (NProgress.status == null) {
+        NProgress.start();
+    }
     $("#home").hide();
     $("#unanswered").show();
-    gapi.client.donate.faqitem.list({"answered":false}).execute(function(items) {
-        var html = "";
+    gapi.client.donate.faqcat.list().execute(function(items){
+        var predrophtml = '<div id="dropdown" class="dropdown"><button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuTitle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">';
+        var postdrophtml = '</button><ul class="dropdown-menu" aria-labelledby="dropdownMenu1">';
+        cats = items.items;
+        cats_by_id = {};
         items.items.forEach(function parseItems(item, index, all) {
-            console.log(item);
-            html += '<div class="jumbotron col-md-4"><form><div class="form-group"><label for="question">Question</label>';
-            html += '<input type="text" class="form-control" id="question" placeholder="Question" value="'+item.question+'"></div>';
-            html += '<div class="form-group"><label for="answer">Answer</label>';
-            html += '<input type="text" class="form-control" id="answer" placeholder="Answer" value="'+item.answer+'"></div>';
-            html += '<div class="form-group"><label for="language">Language-Code</label>';
-            html += '<input type="text" class="form-control" id="language" placeholder="Language-Code" value="'+item.language+'"></div>';
-            html += '<div class="checkbox"><label><input id="answered" type="checkbox"';
-            if(item.answered)
-                html += " checked";
-            html += '>Answered</label></div><input type="hidden" id="id" value="'+item.id+'">';
-            html += '<button class="btn btn-default" id="save'+index+'">Save</button></form></div>';
-            id = "#save"+index
-
+            cats_by_id[item.id] = item;
+        })
+        items.items.forEach(function parseItems(item, index, all) {
+            postdrophtml += '<li class="cat" id="'+item.id+'">'+item.name+'</li>';
         });
-        $("#unanswered").html(html);
-        $("#unanswered").on('click','button',function(e) {
-            //console.log(e);
-            var form = $(e.target).parent();
-            NProgress.start();
-            var question = form.find("#question")[0].value;
-            var answer = form.find("#answer")[0].value;
-            var language = form.find("#language")[0].value;
-            var id = form.find("#id")[0].value;
-            var answered = form.find("#answered")[0].checked;
-            gapi.client.donate.faqitem.update({"id":id,"question":question, "answer":answer, "answered":answered, "language":language}).execute(function(items) {
-                console.log(items);
-                NProgress.done();
+        postdrophtml += '</ul></div>';
+        NProgress.set(0.75)
+        gapi.client.donate.faqitem.list({"answered":false}).execute(function(items) {
+            var html = "";
+            items.items.forEach(function parseItems(item, index, all) {
+                html += '<div class="jumbotron col-md-4"><form><div class="form-group"><label for="question">Question</label>';
+                html += '<input type="text" class="form-control" id="question" placeholder="Question" value="'+item.question+'"></div>';
+                html += '<div class="form-group"><label for="answer">Answer</label>';
+                html += '<input type="text" class="form-control" id="answer" placeholder="Answer" value="'+item.answer+'"></div>';
+                html += '<div class="form-group"><label for="language">Language-Code</label>';
+                html += '<input type="text" class="form-control" id="language" placeholder="Language-Code" value="'+item.language+'"></div>';
+                html += '<div class="checkbox"><label><input id="answered" type="checkbox"';
+                if(item.answered)
+                    html += " checked";
+                html += '>Answered</label></div>';
+                html += '<input type="hidden" id="category" value="'+item.category+'"><input type="hidden" id="id" value="'+item.id+'">';
+                html += predrophtml;
+                if (item.category == undefined) {
+                    html += 'Choose Category<span class="caret"></span>';
+                } else {
+                    html += cats_by_id[item.category].name;
+                }
+                html += postdrophtml;
+                html += '<button class="btn btn-default" id="save">Save</button></form></div>';
+                id = "#save"+index
             });
-        });
-    });  
-
+            $("#unanswered").html(html);
+            NProgress.done();
+        });  
+    });
 }
 jumpToPage();
