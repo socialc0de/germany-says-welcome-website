a5858b2b48c2650a4482a3e70cb8430f74e1dd8e
games647 <games@tsprivat.de>
2015-09-27 14:09:42 +0200

    Basic clean up

--- js/refugee.js
+++ js/refugee.js
@@ -2,10 +2,11 @@ var map;
 function init() {
     var apisToLoad;
     var loadCallback = function() {
-        if (--apisToLoad == 0) {
+        if (--apisToLoad === 0) {
             signin(true, userAuthed);
         }
     };
+
     $("#signInButton").text("Signing in ...");
     apisToLoad = 2;
     apiRoot = 'https://donate-backend.appspot.com/_ah/api';
@@ -15,13 +16,13 @@ function init() {
 
 function signin(mode, authorizeCallback) {
     gapi.auth.authorize({client_id:"760560844994-04u6qkvpf481an26cnhkaauaf2dvjfk0.apps.googleusercontent.com",
-        scope: ["https://www.googleapis.com/auth/plus.login", "https://www.googleapis.com/auth/userinfo.email"], immediate: mode},
+            scope: ["https://www.googleapis.com/auth/plus.login", "https://www.googleapis.com/auth/userinfo.email"],
+            immediate: mode},
         authorizeCallback);
 }
 
 function userAuthed() {
-    var request =
-    gapi.client.oauth2.userinfo.get().execute(function(resp) {
+    var request = gapi.client.oauth2.userinfo.get().execute(function(resp) {
         if (!resp.code) {
             gapi.client.oauth2.user.create().execute(function(resp) {
                 if (!resp.code) {
@@ -34,14 +35,14 @@ function userAuthed() {
 
 function auth() {
     signin(false, userAuthed);
-};
+}
 
 function deauth() {
-    gapi.auth.setToken(null)
+    gapi.auth.setToken(null);
     $("#signInButton").show();
     $("#signInButton").text("Sign in");
     $("#signOutButton").hide();
-};
+}
 
 function signedIn() {
     $("#signInButton").hide();
@@ -51,8 +52,9 @@ function signedIn() {
 
 $(document).ready(function() {
     $(window).bind( 'hashchange', function(e) {
-        jumpToPage()
+        jumpToPage();
     });
+
     $("#newQuestionModal").on('click','#save',function(e) {
         console.log(e);
         //$("#unanswered").html("");
@@ -64,8 +66,9 @@ $(document).ready(function() {
         var language = form.find("#language_code")[0].value;
         var category = form.find("#category")[0].value;
         $("#newQuestionModal").modal('hide');
-        gapi.client.donate.faqitem.create({"question":question, "answer":answer, "language":language, "category":category}).execute(function(resp) {
 
+        var createItems = {"question":question, "answer":answer, "language":language, "category":category};
+        gapi.client.donate.faqitem.create({createItems).execute(function(resp) {
             NProgress.done();
             if (resp.code) {
                 console.log(resp);
@@ -75,6 +78,7 @@ $(document).ready(function() {
             }
         });
     });
+
     $("#newQuestionModal").on('click','.cat',function(e) {
         var p = $(e.target).parent().parent().parent();
         console.log(p);
@@ -85,7 +89,6 @@ $(document).ready(function() {
 });
 
 function jumpToPage() {
-
     var location = window.location.hash;
     if (location.match("^#home")) {
         $('nav').removeClass('fixed');
@@ -114,8 +117,6 @@ function jumpToPage() {
         $('nav a#map_link').parent().addClass('active');
         showMap();
     }
-
-
 }
 
 function loadFAQ() {
@@ -130,60 +131,58 @@ function loadFAQ() {
             var popup_html = '<div id="dropdown" class="dropdown"><button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuTitle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Choose Category</button><ul class="dropdown-menu" aria-labelledby="dropdownMenu1">';
             var askbutton = '<h3>Didn\'t find what you need? <a class="btn btn-primary" data-toggle="modal" data-target="#newQuestionModal">Ask a question!</a></h3>';
 
-            if(items.items == undefined){
+            if(items.items === undefined) {
                 html += askbutton;
                 $("#faq").html(html);
-            }else{
+            } else {
+                items.items.forEach(function parseItems(item, index, all) {
+                    console.log(item);
+                    if (item.category in items_by_cat) {
+                        items_by_cat[item.category].push(item);
+                    } else {
+                        items_by_cat[item.category] = [item];
+                    }
+                });
 
-            items.items.forEach(function parseItems(item, index, all) {
-                console.log(item);
-                if (item.category in items_by_cat) {
-                    items_by_cat[item.category].push(item);
-                } else {
-                    items_by_cat[item.category] = [item];
-                }
-            })
+                html += "<div class=\"panel panel-default index table_of_content\">";
+                html += "<div class=\"panel-body\"><h4>Inhalt</h4>";
 
-            html += "<div class=\"panel panel-default index table_of_content\">";
-            html += "<div class=\"panel-body\"><h4>Inhalt</h4>";
+                cats.items.forEach(function generateHTML(cat, catindex, all) {
+                    items = items_by_cat[cat.id];
+                    if (items != undefined) {
+                        html += '<a href="#faq_' + cat.name + '">' + cat.name + "</a><br />";
+                    }
 
-            cats.items.forEach(function generateHTML(cat, catindex, all) {
-                items = items_by_cat[cat.id];
-                if (items != undefined) {
-                    html += '<a href="#faq_'+cat.name+'">'+cat.name+"</a><br />";
-                }
-                popup_html += '<li class="cat" id="'+cat.id+'">'+cat.name+'</li>';
+                    popup_html += '<li class="cat" id="' + cat.id + '">' + cat.name + '</li>';
+                });
 
-            })
+                html += "</div>";
+                html += "</div>";
 
-            html += "</div>";
-            html += "</div>";
+                cats.items.forEach(function generateHTML(cat, catindex, all) {
+                    items = items_by_cat[cat.id];
+                    if (items != undefined) {
+                        html += '<h2 class="anchor" id="faq_' + cat.name + '">' + cat.name + "</h2>";
+                        html += "<div class=\"panel-group\" id=\"faq_" + catindex + "\" role=\"tablist\" aria-multiselectable=\"true\">";
+                        items.forEach(function addQuestionToHTML(item, index, all) {
+                            html += '<div class="panel panel-default"><div class="panel-heading" role="tab" id="cat' + catindex + 'heading' + index + '"><h4 class="panel-title"><a role="button" data-toggle="collapse" data-parent="cat' + catindex + '" href="#cat' + catindex + 'collapse' + index + '" aria-expanded="false" aria-controls="cat' + catindex + 'collapse' + index + '">';
+                            html += item.question;
+                            html += '</a></h4></div><div id="cat' + catindex + 'collapse' + index + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="cat' + catindex + 'heading' + index + '"><div class="panel-body">';
+                            html += item.answer;
+                            html += '</div></div></div>';
+                        });
 
-            cats.items.forEach(function generateHTML(cat, catindex, all) {
-                items = items_by_cat[cat.id];
-                if (items != undefined) {
-                    html += '<h2 class="anchor" id="faq_'+cat.name+'">'+cat.name+"</h2>";
-                    html += "<div class=\"panel-group\" id=\"faq_"+catindex+"\" role=\"tablist\" aria-multiselectable=\"true\">";
-                    items.forEach(function addQuestionToHTML(item, index, all) {
-                        html += '<div class="panel panel-default"><div class="panel-heading" role="tab" id="cat'+catindex+'heading'+index+'"><h4 class="panel-title"><a role="button" data-toggle="collapse" data-parent="cat'+catindex+'" href="#cat'+catindex+'collapse'+index+'" aria-expanded="false" aria-controls="cat'+catindex+'collapse'+index+'">';
-                        html += item.question;
-                        html += '</a></h4></div><div id="cat'+catindex+'collapse'+index+'" class="panel-collapse collapse" role="tabpanel" aria-labelledby="cat'+catindex+'heading'+index+'"><div class="panel-body">';
-                        html += item.answer;
-                        html += '</div></div></div>';
-                    })
-                    html += '</div>';
-                }
+                        html += '</div>';
+                    }
+                });
 
-            })
-            html += askbutton;
-            $("#faq").html(html);
-            $("#newQuestionModalText").append(popup_html);
+                html += askbutton;
+                $("#faq").html(html);
+                $("#newQuestionModalText").append(popup_html);
             }
         });
     });
-
 }
-
 
 function loadSharing() {
     $("#home").hide();
@@ -191,12 +190,14 @@ function loadSharing() {
     $("#faq").hide();
     $("#map_container").hide();
 }
+
 function showHome() {
     $("#home").show();
     $("#sharing").hide();
     $("#faq").hide();
     $("#map_container").hide();
 }
+
 function showMap() {
     $("#home").hide();
     $("#sharing").hide();
@@ -204,46 +205,49 @@ function showMap() {
     $("#map_container").show();
     loadMapIfNeeded();
 }
+
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
-
     L.circle(e.latlng, radius).addTo(map);
 }
+
 function loadMapData() {
     //load authorities
-  $.getJSON('https://raw.githubusercontent.com/germany-says-welcome/refugees-welcome-app/master/app/src/main/assets/authorities.json', function(data) {
-    data.forEach(function (entry) {
-      var popup = '<a href="geo:' + entry.location.lat + ', ' + entry.location.lng + '">' + entry.adresse + ' </a> <br />';
-      popup += entry.telefon ? '<i class="glyphicon glyphicon-phone-alt"></i> <a href="tel:+49' + entry.telefon + '">' + entry.telefon + '</a>' + '   ' : '';
-      popup += entry.fax ? '<i class="glyphicon glyphicon-print"></i> ' + entry.fax + '<br />' : '';
-      popup += entry.offnungszeiten ? '<i class="glyphicon glyphicon glyphicon-time"></i> ' + entry.offnungszeiten + '<br />' : '';
-      popup += entry.website ? '<i class="glyphicon glyphicon-info-sign"></i> <a href="http://' + entry.website + '">' + entry.website + '</a><br />' : '';
-      popup += 'Data from <a href="http://www.amt-de.com">www.amt-de.com';
+    var authortiesUrl = 'https://raw.githubusercontent.com/germany-says-welcome/refugees-welcome-app/master/app/src/main/assets/authorities.json';
+    $.getJSON(authortiesUrl, function(data) {
+        data.forEach(function (entry) {
+        var popup = '<a href="geo:' + entry.location.lat + ', ' + entry.location.lng + '">' + entry.adresse + ' </a> <br />';
+        popup += entry.telefon ? '<i class="glyphicon glyphicon-phone-alt"></i> <a href="tel:+49' + entry.telefon + '">' + entry.telefon + '</a>' + '   ' : '';
+        popup += entry.fax ? '<i class="glyphicon glyphicon-print"></i> ' + entry.fax + '<br />' : '';
+        popup += entry.offnungszeiten ? '<i class="glyphicon glyphicon glyphicon-time"></i> ' + entry.offnungszeiten + '<br />' : '';
+        popup += entry.website ? '<i class="glyphicon glyphicon-info-sign"></i> <a href="http://' + entry.website + '">' + entry.website + '</a><br />' : '';
+        popup += 'Data from <a href="http://www.amt-de.com">www.amt-de.com';
 
-      //bind the authority to it's category
-      L.marker([entry.location.lat, entry.location.lng]).addTo(authorities)
-        .bindPopup(popup);
-    });
-  });
-
-  //load wifi hotspots
-  $.ajax({
-    type: "GET",
-    url: "http://www.freifunk-karte.de/fetch.php?content=gpxfile",
-    dataType: "xml",
-    success: function (xml) {
-        $(xml).find("wpt").each(function() {
-            var longitude = $(this).attr("lon");
-            var latitude = $(this).attr("lat");
-            var name = $(this).find("name").first().text();
-
-            var marker = L.marker([latitude, longitude]);
-            marker.bindPopup(name);
-            wifi.addLayer(marker);
+        //bind the authority to it's category
+        L.marker([entry.location.lat, entry.location.lng]).addTo(authorities)
+            .bindPopup(popup);
         });
-      }
-  });
+    });
+
+    //load wifi hotspots
+    $.ajax({
+        type: "GET",
+        url: "http://www.freifunk-karte.de/fetch.php?content=gpxfile",
+        dataType: "xml",
+        success: function (xml) {
+            $(xml).find("wpt").each(function() {
+                var longitude = $(this).attr("lon");
+                var latitude = $(this).attr("lat");
+                var name = $(this).find("name").first().text();
+
+                var marker = L.marker([latitude, longitude]);
+                marker.bindPopup(name);
+                wifi.addLayer(marker);
+            });
+        }
+    });
 }
+
 function loadMap() {
     //create layer groups in order to be accessible from loadMapData
     authorities = L.layerGroup();
@@ -251,38 +255,40 @@ function loadMap() {
 
     //cologne as default location
     map = L.map('map', {
-    center: [50.9485795, 6.9448561],
-    //default zoom state
-    zoom: 13,
-    //just use authorities as default layer
-    layers: [authorities]
+        center: [50.9485795, 6.9448561],
+        //default zoom state
+        zoom: 13,
+        //just use authorities as default layer
+        layers: [authorities]
     });
 
     //selectable layers
     var overlayMaps = {
-    "Authorities": authorities,
-    "Wifi": wifi
+        "Authorities": authorities,
+        "Wifi": wifi
     };
 
     L.control.layers(overlayMaps).addTo(map);
 
-    mapLink =
-        '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+    mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
     L.tileLayer(
         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; ' + mapLink + ' Contributors',
-        maxZoom: 18,
-        }).addTo(map);
+        maxZoom: 18
+    }).addTo(map);
+
     map.locate({setView: true, maxZoom: 16});
     //switch to current gps position if found
     map.on('locationfound', onLocationFound);
     loadMapData();
 }
+
 function loadMapIfNeeded() {
-    if (map == undefined) {
+    if (map === undefined) {
         loadMap();
     }
 }
+
 $("#map_container").hide();
 
 jumpToPage();
--- js/volunteer.js
+++ js/volunteer.js
@@ -4,11 +4,11 @@ function init() {
     var apisToLoad;
     NProgress.start();
     var loadCallback = function() {
-        
         if (--apisToLoad == 0) {
             signin(true, userAuthed);
         }
     };
+
     $("#signInButton").text("Signing in ...");
     apisToLoad = 2;
     apiRoot = 'https://donate-backend.appspot.com/_ah/api';
@@ -17,7 +17,8 @@ function init() {
 }
 function signin(mode, authorizeCallback) {
     gapi.auth.authorize({client_id:"760560844994-04u6qkvpf481an26cnhkaauaf2dvjfk0.apps.googleusercontent.com",
-        scope: ["https://www.googleapis.com/auth/plus.login", "https://www.googleapis.com/auth/userinfo.email"], immediate: mode},
+            scope: ["https://www.googleapis.com/auth/plus.login", "https://www.googleapis.com/auth/userinfo.email"],
+            immediate: mode},
         authorizeCallback);
 }
 function userAuthed() {
@@ -34,21 +35,23 @@ function userAuthed() {
                     } else {
                         showUserNotVolunteerOrAdmin();
                     }
-                    
                 }
             });
         }
     });
 }
+
 function auth() {
     signin(false, userAuthed);
-};
+}
+
 function deauth() {
     gapi.auth.setToken(null)
     $("#signInButton").show();
     $("#signInButton").text("Sign in");
     $("#signOutButton").hide();
-};
+}
+
 function signedIn() {
     $("#signInButton").hide();
     $("#signOutButton").show();
@@ -57,13 +60,13 @@ function signedIn() {
 $(document).ready(function() {
     setQuestionListener("#answered")
     setQuestionListener("#unanswered")
-
 });
 
 function ucfirst(str) {
     var firstLetter = str.substr(0, 1);
     return firstLetter.toUpperCase() + str.substr(1);
 }
+
 function setQuestionListener(tag_id) {
     $(tag_id).on('click', "#save", function(e) {
         $(tag_id).html("");
@@ -76,7 +79,10 @@ function setQuestionListener(tag_id) {
         var id = form.find("#id")[0].value;
         var answered = form.find("#answered")[0].checked;
         var category = form.find("#category")[0].value;
-        gapi.client.donate.faqitem.update({"id":id,"question":question, "answer":answer, "answered":answered, "language":language, "category":category}).execute(function(resp) {
+
+        var updateItems = {"id":id, "question":question, "answer":answer, "answered":answered
+            , "language":language, "category":category};
+        gapi.client.donate.faqitem.update(updateItems).execute(function(resp) {
             NProgress.set(0.5);
             if (!resp.code) {
                 window["show"+ucfirst(tag_id.substring(1))]();
@@ -86,12 +92,12 @@ function setQuestionListener(tag_id) {
                 $('#errorModalLabel').append("Error Code "+resp.code);
                 $('#errorModal').modal();
             }
-
         });
     });
+
     $(tag_id).on('click', "#delete", function(e) {
         $(tag_id).html("");
-        //
+
         var form = $(e.target).parent();
         NProgress.start();
         var id = form.find("#id")[0].value;
@@ -102,14 +108,15 @@ function setQuestionListener(tag_id) {
                 //showUnanswered();
             } else {
                 console.log(resp);
-                $('#errorModalText').text("Error: "+resp.message);
-                $('#errorModalLabel').append("Error Code "+resp.code);
+                $('#errorModalText').text("Error: " + resp.message);
+                $('#errorModalLabel').append("Error Code " + resp.code);
                 $('#errorModal').modal();
             }
 
         });
     });
-    $(tag_id).on('click','.cat',function(e) {
+
+    $(tag_id).on('click', '.cat',function(e) {
         var p = $(e.target).parent().parent().parent();
         p.find("#category").prop('value', e.target.id);
         p.find("#dropdownMenuTitle").text(e.target.textContent);
@@ -131,17 +138,20 @@ function loadQuestions(answered) {
     if (NProgress.status == null) {
         NProgress.start();
     }
-    gapi.client.donate.faqcat.list().execute(function(items){
+
+    gapi.client.donate.faqcat.list().execute(function(items) {
         var predrophtml = '<div id="dropdown" class="dropdown"><button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenuTitle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">';
         var postdrophtml = '</button><ul class="dropdown-menu" aria-labelledby="dropdownMenu1">';
         cats = items.items;
         cats_by_id = {};
         items.items.forEach(function parseItems(item, index, all) {
             cats_by_id[item.id] = item;
-        })
-        items.items.forEach(function parseItems(item, index, all) {
-            postdrophtml += '<li class="cat" id="'+item.id+'">'+item.name+'</li>';
         });
+
+        items.items.forEach(function parseItems(item, index, all) {
+            postdrophtml += '<li class="cat" id="' + item.id + '">' + item.name + '</li>';
+        });
+
         postdrophtml += '</ul></div>';
         NProgress.set(0.75)
         gapi.client.donate.faqitem.list({"answered":answered}).execute(function(items) {
@@ -149,22 +159,24 @@ function loadQuestions(answered) {
             if (items.items != undefined) {
                 items.items.forEach(function parseItems(item, index, all) {
                     html += '<div class="jumbotron col-md-4"><div><div class="form-group"><label for="question">Question</label>';
-                    html += '<input type="text" class="form-control" id="question" placeholder="Question" value="'+item.question+'"></div>';
+                    html += '<input type="text" class="form-control" id="question" placeholder="Question" value="' + item.question + '"></div>';
                     html += '<div class="form-group"><label for="answer">Answer</label>';
-                    html += '<input type="text" class="form-control" id="answer" placeholder="Answer" value="'+item.answer+'"></div>';
+                    html += '<input type="text" class="form-control" id="answer" placeholder="Answer" value="' + item.answer + '"></div>';
                     html += '<div class="form-group"><label for="language">Language-Code</label>';
-                    html += '<input type="text" class="form-control" id="language" placeholder="Language-Code" value="'+item.language+'"></div>';
+                    html += '<input type="text" class="form-control" id="language" placeholder="Language-Code" value="' + item.language + '"></div>';
                     html += '<div class="checkbox"><label><input id="answered" type="checkbox"';
                     if(item.answered)
                         html += " checked";
+
                     html += '>Answered</label></div>';
-                    html += '<input type="hidden" id="category" value="'+item.category+'"><input type="hidden" id="id" value="'+item.id+'">';
+                    html += '<input type="hidden" id="category" value="' + item.category+'"><input type="hidden" id="id" value="' + item.id + '">';
                     html += predrophtml;
                     if (item.category == undefined) {
                         html += 'Choose Category<span class="caret"></span>';
                     } else {
                         html += cats_by_id[item.category].name;
                     }
+
                     html += postdrophtml;
                     html += '<button class="btn btn-default" id="save">Save</button><button class="btn btn-default" id="delete">Delete</button></div></div>';
                 });
@@ -177,6 +189,7 @@ function loadQuestions(answered) {
         });
     });
 }
+
 function showUnanswered() {
     $("#home").hide();
     $("#answered").hide();
