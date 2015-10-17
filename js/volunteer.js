354f044df8b8b06c00378c37c7a64e40d2e13ef8
Breuxi <breuxi.innovations@gmail.com>
2015-10-17 20:59:30 +0200

    Adding Translation Switch to Volunteer.html

--- js/volunteer.js
+++ js/volunteer.js
@@ -84,6 +84,30 @@ function signedIn() {
 $(document).ready(function () {
   setQuestionListener('#answered');
   setQuestionListener('#unanswered');
+
+  var option = {
+    fallbackLng: 'en',
+    ns: {
+      namespaces: ['volunteer']
+    },
+    detectLngQS: 'lang'
+  };
+
+  $.i18n.init(option)
+      .done(function () {
+        $('[data-i18n]').i18n();
+      })
+      .fail(function () {
+        $('[data-i18n]').i18n();
+      });
+
+
+  $('#lang-select li[lang]').on('click', function() {
+    var lang = $(this).attr('lang');
+    $.i18n.setLng(lang, function(){
+      $('[data-i18n]').i18n();
+    });
+  });
 });
 
 function ucfirst(str) {
