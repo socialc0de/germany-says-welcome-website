3094a02946d1a24b86a8b5b020eea536b032fb55
Breuxi <breuxi.innovations@gmail.com>
2015-10-17 16:47:56 +0200

    Refugee.html Translation

--- js/refugee.js
+++ js/refugee.js
@@ -313,6 +313,30 @@ function loadSharingMapData() {
   });
 }
 $(document).ready(function () {
+  var option = {
+    fallbackLng: 'en',
+    ns: {
+      namespaces: ['refugee']
+    },
+    detectLngQS: 'lang'
+  };
+
+  $.i18n.init(option)
+      .done(function () {
+        $('[data-i18n]').i18n();
+      })
+      .fail(function () {
+        $('[data-i18n]').i18n();
+      });
+
+
+  $('#lang-select li[lang]').on('click', function() {
+    var lang = $(this).attr('lang');
+    $.i18n.setLng(lang, function(){
+      $('[data-i18n]').i18n();
+    });
+  });
+
   $("#newQuestionModal").on('click', '#save', function (e) {
     console.log(e);
     var form = $(e.target).parent().parent();
