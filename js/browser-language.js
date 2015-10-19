855a16284c03ea12ce19403982ff8886e53d4b01
Breuxi <breuxi.innovations@gmail.com>
2015-10-19 20:42:54 +0200

    New Language Dropdown

--- js/browser-language.js
+++ js/browser-language.js
@@ -1,14 +1,21 @@
 $(document).ready(function () {
    var browserlang = navigator.language;
 
+
    if(browserlang.match("de")){
       $.i18n.setLng('de', function(){
          console.log("Websitesprache: de");
+         $("#de").addClass("active");
+         $("#flag_de").show();
+         $("#flag_en").hide();
          $('[data-i18n]').i18n();
       });
    }else{
       $.i18n.setLng('en', function(){
          console.log("Websitesprache: en");
+         $("#en").addClass("active");
+         $("#flag_de").hide();
+         $("#flag_en").show();
          $('[data-i18n]').i18n();
       });
    }
