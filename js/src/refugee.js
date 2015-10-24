a439d918c7bee1ae8e6b6625fdaa66777f1f2768
Stefan Keil <an.stefan.keil@googlemail.com>
2015-10-24 17:56:53 +0200

    refactoring language picker

--- js/src/refugee.js
+++ js/src/refugee.js
@@ -1,3 +1,5 @@
+import langPicker from './refugee/language-picker'
+
 //Reservierte globale Variablen
 var map;
 var sharingMap;
@@ -421,27 +423,7 @@ $(document).ready(function () {
         $('[data-i18n]').i18n();
       });
 
-
-  $('#lang-select li[lang]').on('click', function() {
-    var lang = $(this).attr('lang');
-
-    if(lang == "de"){
-      $("#flag_de").show();
-      $("#flag_en").hide();
-    }
-
-    if(lang == "en"){
-      $("#flag_de").hide();
-      $("#flag_en").show();
-    }
-
-
-    $('#lang-select li[lang]').removeClass("active");
-    $(this).addClass("active");
-    $.i18n.setLng(lang, function(){
-      $('[data-i18n]').i18n();
-    });
-  });
+  langPicker()
 
   //Dialog-Handling für neue Fragen
   $("#newQuestionModal").on('click', '#save', function (e) {
---
+++ js/src/refugee/language-picker.js
@@ -0,0 +1,23 @@
+//Sprachenauswahl
+
+export default function langPicker () {
+  $('#lang-select li[lang]').on('click', function() {
+    var lang = $(this).attr('lang');
+
+    if(lang == "de"){
+      $("#flag_de").show();
+      $("#flag_en").hide();
+    }
+
+    if(lang == "en"){
+      $("#flag_de").hide();
+      $("#flag_en").show();
+    }
+
+
+    $('#lang-select li[lang]').removeClass("active");
+    $(this).addClass("active");
+    $.i18n.setLng(lang, function(){
+      $('[data-i18n]').i18n();
+    });}
+}
