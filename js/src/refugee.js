dba1dcddef482e4b4083bbbe0bc8153912cad4c0
Stefan Keil <an.stefan.keil@googlemail.com>
2015-10-25 12:59:32 +0100

    Move to tiny-react-router because of incompatible react dependency

--- js/src/refugee.js
+++ js/src/refugee.js
@@ -1,88 +1,17 @@
-/* global $, gapi, NProgress */
+/* global $ */
 
-import router from './refugee/router'
-import langPicker from './refugee/language-picker'
 import loginInit from './refugee/login-button'
 
-import homeController from './refugee/home'
-import faqController from './refugee/faq'
-import mapController from './refugee/map'
-
 import React from 'react'
+import { render } from 'react-dom'
 import App from './refugee/components/app'
 
+// global callback for google signin
 window.auth = function auth () {
-  // signin(false, userAuthed);
-  // TMP Fix, init() doesn't get called by client.js
   loginInit()
 }
 
-// Sprachenauswahl
-/*
-$(document).ready(function () {
-  var option = {
-    fallbackLng: 'en',
-    ns: {
-      namespaces: ['refugee']
-    },
-    detectLngQS: 'lang'
-  }
-
-  $.i18n.init(option)
-      .done(function () {
-        $('[data-i18n]').i18n()
-      })
-      .fail(function () {
-        $('[data-i18n]').i18n()
-      })
-
-  langPicker()
-
-  // Dialog-Handling für neue Fragen
-  $('#newQuestionModal').on('click', '#save', function (e) {
-    var form = $(e.target).parent().parent()
-    NProgress.start()
-    var question = form.find('#question_text')[0].value
-    var answer = form.find('#answer_text')[0].value
-    var language = form.find('#language_code')[0].value
-    var category = form.find('#category')[0].value
-    $('#newQuestionModal').modal('hide')
-
-    var createItems = {'question': question, 'answer': answer, 'language': language, 'category': category}
-    gapi.client.donate.faqitem.create(createItems).execute(function (resp) {
-      NProgress.done()
-      if (resp.code) {
-        console.log(resp)
-        $('#errorModalText').text('Error: ' + resp.message)
-        $('#errorModalLabel').text('Error Code ' + resp.code)
-        $('#errorModal').modal()
-      }
-    })
-  })
-
-  $('#newQuestionModal').on('click', '.cat', function (e) {
-    var p = $(e.target).parent().parent().parent()
-    console.log(p)
-    console.log(p)
-    p.find('#category').prop('value', e.target.id)
-    p.find('#dropdownMenuTitle').text(e.target.textContent)
-  })
-
-  router($('#bs-example-navbar-collapse-1'), {
-    showHome: homeController.show,
-    showMap: mapController.show,
-    showFAQ: faqController.show
-  })
-})
-*/
-
+// mount the react app and render it as soon as our document is ready
 $(document).ready(() => {
-  // debugger
-  React.render(
-    // we can not use html5 history as of now because our root is a refugee.html
-    React.createFactory(App)({ history: false }),
-    document.getElementById('content')
-  )
+  render(App, document.getElementById('content'))
 })
-
-$('#map_container').hide()
--- js/src/refugee/components/app.js
+++ js/src/refugee/components/app.js
@@ -1,35 +1,13 @@
 import React from 'react'
-import { RouterMixin } from 'react-mini-router'
+import Router from 'tiny-react-router'
 
 // all pages are organized as separate components
 import Dashboard from './dashboard'
 import Map from './map'
 
-var App = React.createClass({
+var routes = {
+  '/': Dashboard,
+  '/map': Map
+}
 
-  mixins: [ RouterMixin ],
-
-  routes: {
-    '/': 'home',
-    '/map': 'map'
-  },
-
-  home () {
-    return <Dashboard />
-  },
-
-  map () {
-    return <Map />
-  },
-
-  notFound (path) {
-    return <p>Page Not Found: {path}</p>
-  },
-
-  render () {
-    return <div>{this.renderCurrentRoute()}</div>
-  }
-
-})
-
-export default App
+export default <Router routes={routes} />
