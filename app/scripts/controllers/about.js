b227946ca00338ad7566267dc2a415d4571bf798
pajowu <github@ca.pajowu.de>
2015-10-31 13:29:01 +0200

    init repo with yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leafletApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leafletApp
+ */
+angular.module('leafletApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
